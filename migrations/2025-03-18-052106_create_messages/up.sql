c9e284215f2350fd60bf14cbe076a8c98910a9af
Quentin Tennerel <tennerelquentin@gmail.com>
2025-03-20 14:27:05 +0100

    Feat : add some tables for the messaging database

--- migrations/2025-03-18-052106_create_messages/up.sql
+++ migrations/2025-03-18-052106_create_messages/up.sql
@@ -1,9 +1,33 @@
 -- Your SQL goes here
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(320) UNIQUE NOT NULL,
+    password VARCHAR(320) NOT NULL
+);
+
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
-    sender VARCHAR(320) NOT NULL,  -- Sender's ID or username
+    sender INT REFERENCES users(id),
     recipient VARCHAR(320) NOT NULL, -- Supports group messages
     content TEXT,  -- Message text content
     is_read BOOLEAN DEFAULT FALSE,
     sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    type enum('message', 'response') DEFAULT 'message'
+    response_to INT REFERENCES messages(id) -- Only for response messages
+);
+
+CREATE TABLE conversations (
+    id SERIAL PRIMARY KEY,
+);
+
+CREATE TABLE conversation_participants (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    conversation_id INT REFERENCES conversations(id)
+    type enum('group', 'private') DEFAULT 'private'
+    name VARCHAR(320) -- Only for group conversations
+);
+
+CREATE TABLE conversation_messages (
+    conversation_id INT REFERENCES conversations(id),
 );
